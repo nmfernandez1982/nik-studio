08e1658eb8f72e0935bacd0e933e5f50d4d0e8b1
Nicolas <nmfernandez1982@gmail.com>
2026-05-09 08:14:50 -0300

    Commit

--- enviar_correo.php
+++ enviar_correo.php
@@ -11,7 +11,7 @@ $mensaje = $_POST["mensaje"];
 
 $smtpHost    = "c2701652.ferozo.com";
 $smtpUsuario = "comercial@nik-studio.com.ar";
-$smtpClave   = "TU_NUEVA_CONTRASEÑA"; // ← cambiala
+$smtpClave   = "XeBh7l*0"; // ← cambiala
 $emailDestino = "comercial@nik-studio.com.ar";
 
 $mail = new PHPMailer();
